4137d26cfb9f5f9bb86afe32675f75dff2fc4e50
Thiago de Melo Lima <remediosdemaispodcast@gmail.com>
2024-06-06 14:57:34 -0300

    CarboReferencia update

--- js/index.js
+++ js/index.js
@@ -66,11 +66,11 @@ $(window).on("load", function ()
         }
         else if (OpSelecionada == 2)
         {
-            CarboReferencia = 15;
+            CarboReferencia = 12;
         }
         else 
         {
-            CarboReferencia = 18;
+            CarboReferencia = 15;
         }
 
         return CarboReferencia;
