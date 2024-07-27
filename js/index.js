6665c3fd1d55be499785d4b85f1849f0b6a8aee3
Thiago de Melo Lima <remediosdemaispodcast@gmail.com>
2024-07-27 14:41:01 -0300

    CarboReferencia manhã alterado

--- js/index.js
+++ js/index.js
@@ -62,7 +62,7 @@ $(window).on("load", function ()
 
         if(OpSelecionada == 1)
         {
-            CarboReferencia = 6;
+            CarboReferencia = 8;
         }
         else if (OpSelecionada == 2)
         {
